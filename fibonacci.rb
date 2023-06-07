d85ee629cdfbcd9d83dfffb23eb0b3dc305dafdb
Wes Hendley <wrhendley@gmail.com>
2023-06-07 13:33:20 -0500

    Add fibs_rec method, which is recursive

--- fibonacci.rb
+++ fibonacci.rb
@@ -9,6 +9,17 @@ def fibs(num)
   fib_arr
 end
 
-puts "Enter a fib number"
-fib_size = gets.chomp.to_i
-p fibs(fib_size)
+#-----------------------------------------
+
+def fibs_rec(num)
+  return [0] if num == 1
+  return [0, 1] if num == 2
+
+  fib_arr = fibs_rec(num - 1)
+  fib_arr << fib_arr[-2] + fib_arr[-1]
+  fib_arr
+end
+
+puts 'Enter a fib number'
+fib_size = $stdin.gets.chomp.to_i
+p fibs_rec(fib_size)
